f4e138d885a2d054467c24d6d48bc3957ab3b8db
rsureka <rsureka@paypal.com>
2025-08-01 18:52:35 +0530

    Learned URLSession.shared.dataTask image download

---
+++ Swift-Concurrency/DownloadImageAsync.swift
@@ -0,0 +1,77 @@
+//
+//  DownloadImageAsync.swift
+//  Swift-Concurrency
+//
+//  Created by Rahul Sureka on 01/08/25.
+//
+
+import SwiftUI
+
+/*
+ What we are learning here?
+ - First we learn how to download image using escaping closure and combine.
+ - Then we learn how to download image using async await.
+ */
+
+class DownloadImageAsyncImageLoader {
+  
+  let url = URL(string: "https://picsum.photos/200")!
+  func downloadwithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> Void) {
+    // This function would typically download an image asynchronously using a closure.
+    
+    // URLSession.shared.dataTask is an asynchronous method that fetches data from the specified URL.
+    // Its returns data, response, and error in the completion handler.
+    // This is a escaping closure because it is called after the function returns.
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      guard
+        let data = data,
+        let image = UIImage(data: data),
+        let response = response as? HTTPURLResponse,
+        response.statusCode >= 200 && response.statusCode < 300 else {
+        return
+      }
+      completionHandler(image, nil)
+      
+    }
+    .resume()
+    //creates a task, but doesn’t start it. To actually execute the network call, you must call .resume():
+  }
+}
+
+class DownloadImageAsyncViewModel: ObservableObject {
+  @Published var image: UIImage? = nil
+  let loader = DownloadImageAsyncImageLoader()
+  
+  func fetchImage() {
+    loader.downloadwithEscaping { [weak self] image, error in
+      //We added the image update in main thread because UIKit updates must be done on the main thread.
+      //The operation URLSession.shared.dataTask is done on a background thread, so we need to switch back to the main thread to update the UI.
+      DispatchQueue.main.async {
+        self?.image = image
+      }
+    }
+  }
+}
+
+
+struct DownloadImageAsync: View {
+  @StateObject private var viewModel = DownloadImageAsyncViewModel()
+  
+    var body: some View {
+      ZStack {
+        if let image = viewModel.image {
+          Image(uiImage: image)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 250, height: 250)
+        }
+      }
+      .onAppear {
+        viewModel.fetchImage()
+      }
+    }
+}
+
+#Preview {
+    DownloadImageAsync()
+}
